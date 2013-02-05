a00c3c3bc5f922a31c0b322bf6daa1f786611ed7
Allan Davis <cajun.code@gmail.com>
2013-02-05 12:12:34 -0500

    Monkey Patch HTTPI Net::HTTP adaptor to pass ssl version

--- lib/acculynk/client.rb
+++ lib/acculynk/client.rb
@@ -3,6 +3,40 @@ require 'acculynk/configuration'
 require 'acculynk/request'
 require 'savon'
 
+
+module HTTPI
+  module Adapter
+    class NetHTTP < Base
+      alias request_old request
+      
+      def request(method)
+        unless REQUEST_METHODS.include? method
+          raise NotSupportedError, "Net::HTTP does not support custom HTTP methods"
+        end
+
+        do_request(method) do |http, http_request|
+          http.ssl_version = :SSLv3
+          http_request.body = @request.body
+          if @request.on_body then
+            http.request(http_request) do |res|
+              res.read_body do |seg|
+                @request.on_body.call(seg)
+              end
+            end
+          else
+            http.request http_request
+          end
+        end
+      rescue OpenSSL::SSL::SSLError
+        raise SSLError
+      rescue Errno::ECONNREFUSED   # connection refused
+        $!.extend ConnectionError
+        raise
+      end
+    end
+  end
+end
+
 module Acculynk
 
   class Client
